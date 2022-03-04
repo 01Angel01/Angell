e7875f4aa3abf3bb40d18a3113613973e9e7a731
Angelina Yessyca Rahardjo Hadi <90541566+ComicalBanana@users.noreply.github.com>
2022-03-04 17:51:31 +0700

    Add files via upload

---
+++ Angel/Angel/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Angel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string nama = textBox1.Text;
+            MessageBox.Show("Halo " + nama + "\n Semangat " + nama + " Kamu pasti bisa ^ ^");
+        }
+    }
+}
